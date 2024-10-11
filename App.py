ba50aa9de5327fc7b027f7a74b6ad910d365784a
Ark Sharma <145752746+Arksgithub@users.noreply.github.com>
2024-10-11 09:51:17 +0530

    Update App.py

--- App.py
+++ App.py
@@ -4,7 +4,7 @@ import pandas as pd
 import requests
 
 def fetch_poster(movie_id):
-    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=74fa9a357031f2bd39373cf70640e4fe'.format(movie_id))
+    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key="API KEY"'.format(movie_id))
     data = response.json()
     return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
 def recommend(movie):
